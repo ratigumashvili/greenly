186dec0d7eaec813ce6f0cd9e9d637cef36c048b
ratigumashvili <ratigumashvili@gmail.com>
2025-02-16 15:46:43 +0400

    started working on navbar

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
+import { SearchIcon } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export function Navbar() {
     return (
-        <div className="w-full max-w-7xl mx-auto flex items-center justify-between px-4 md:px-8 lg:px-10 py-4">
-            <Image src={'/logo-lg.svg'} width={150} height={150} alt="Logo" />
-        </div>
+        <nav className="h-[10vh] w-full max-w-7xl mx-auto flex items-center justify-between px-4 md:px-8 lg:px-10 border-b">
+            <Link href={'/'}>
+                <Image
+                    src={'/logo-lg.svg'}
+                    width={150}
+                    height={150}
+                    alt="Logo"
+                />
+            </Link>
+            <div className="relative w-full max-w-[400px] ">
+                <Input
+                    placeholder="Search greenly"
+                    className="rounded-full pl-9 placeholder:text-muted-foreground bg-slate-50"
+                />
+                <SearchIcon
+                    className="size-5 absolute top-1/2 left-2 -translate-y-1/2 text-muted-foreground"
+                />
+            </div>
+            <div className="flex items-center gap-x-2">
+                <Button>Register</Button>
+                <Button variant="outline">Login</Button>
+            </div>
+        </nav>
     )
 }
