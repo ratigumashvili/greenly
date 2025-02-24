baa0725f06c67a3c68eabdb19b37b2c513190937
ratigumashvili <ratigumashvili@gmail.com>
2025-02-24 09:25:22 +0400

    cleanup

--- app/g/[id]/page.tsx
+++ app/g/[id]/page.tsx
@@ -1,21 +1,8 @@
-// import Link from "next/link"
-// import { SettingsIcon } from "lucide-react"
-
-// import { Badge } from "@/components/ui/badge"
-// import { Separator } from "@/components/ui/separator"
-// import { Button } from "@/components/ui/button"
 import { PageTitle } from "@/components/shared/page-title"
-// import { CreatedAt } from "@/components/shared/created-at"
-// import { SubcommunityMemberList } from "@/components/shared/subcommunity-member-list"
 import { Feed } from "@/components/shared/feed/feed"
+import { AboutTheCommunity } from "@/components/shared/about-the-community"
 
 import { prisma } from "@/lib/prisma"
-// import { getUserData } from "@/lib/utils"
-// import { JoinCommunityButton } from "@/components/forms/join-community-button"
-import { AboutTheCommunity } from "@/components/shared/about-the-community"
-import { getUserData } from "@/lib/utils";
-
-
 
 async function getData(id: string) {
     const data = await prisma.subcommunity.update({
@@ -32,15 +19,9 @@ async function getData(id: string) {
     return data;
 }
 
-
 export default async function SingleSubCommunityPage({ params }: { params: { id: string } }) {
     const { id } = await params
     const data = await getData(id)
-    const { session, user } = await getUserData(id)
-
-    // const isMember = user ? data.members.some(member => member.userId === user.id) : false;
-    // const isCreator = user?.email === data.User?.email;
-    // const isAdmin = user?.SubcommunityMember[0]?.role === "admin"
 
     if (!data) {
         return null
